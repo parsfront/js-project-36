ccec947c9e08503aae43c8f16f66bd15a5098afa
Ali Bahadoran <alibahadoran.eng@gmail.com>
2022-07-14 12:35:35 +0430

    Header

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let header = document.querySelector(".header");
+let sticky = header.offsetTop;
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+});
